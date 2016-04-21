5d59957fc2e4ffdfa4e5efa41c9f5292ef8bc348
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-04-21 14:50:24 -0500

    Created seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,16 @@
 # For example:
 #
 # Insect.create!(name: "Fuzzy Mantis", description: "Really Fierce")
+
+
+Wolf.create!(name: "Scott Howard", description: "I always wanted to be special...but 
+      I never expected this.", image_url: "https://cinefilesreviews.files.wordpress.com/2015/11/teen-wolf-michael-j-fox-movie-review-1985.jpg")
+
+Wolf.create!(name: "Will Randall", description: "The animal is out.", image_url: "https://i.ytimg.com/vi/TmVHIh3vEgQ/maxresdefault.jpg")
+
+Wolf.create!(name: "Larry Talbot", description: "Even a man who is pure in heart, and says his prayers by night;
+May become a wolf when the wolfbane blooms and the autumn moon is bright", image_url: "http://www.meredy.com/chaneyjrlon03.jpg")
+
+Wolf.create!(name: "Lawrence Talbot", description: "I will kill all of you!", image_url: "http://media.aintitcool.com/media/legacy/images2007/RB-BDT.jpg")
+
+Wolf.create!(name: "David Kessler", description: "Maybe it's a sheep dog... let's keep going.", image_url: "http://www.bfi.org.uk/sites/bfi.org.uk/files/styles/full/public/image/american-werewolf-in-london-an-1981-004-wolf-groan.jpg?itok=64_A8LQt")
